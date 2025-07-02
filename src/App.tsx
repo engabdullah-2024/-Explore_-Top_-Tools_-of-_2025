a0737902fb35343913c77be04b88574d3c0fb2b6
EngIltire <iltirepoi@gmail.com>
2025-07-02 18:16:03 +0300

    updated in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // create modern app routes with Home route
-import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.js';
 
